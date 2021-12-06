b9ba0e9048d009e677d487611cb0ba492c421575
darco2018 <newsecond@outlook.com>
2021-12-06 16:01:26 +0100

    initial commit

---
+++ src/main/java/com/ust/wordmaster/WordMasterApplication.java
@@ -0,0 +1,13 @@
+package com.ust.wordmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ust/wordmaster/WordMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.wordmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
