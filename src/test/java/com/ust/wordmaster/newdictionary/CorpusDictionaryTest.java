0dd491148025eaf72f49950d95cbc3dd72a2f370
darco2018 <newsecond@outlook.com>
2021-12-17 19:43:11 +0100

    add skeleton test for CorpusDictionary

---
+++ src/test/java/com/ust/wordmaster/newdictionary/CorpusDictionaryTest.java
@@ -0,0 +1,24 @@
+package com.ust.wordmaster.newdictionary;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CorpusDictionaryTest {
+
+    @Test
+    void getDictionary() {
+    }
+
+    @Test
+    void getDictionarySubset() {
+    }
+
+    @Test
+    void containsEntry() {
+    }
+
+    @Test
+    void getDictionaryEntry() {
+    }
+}
