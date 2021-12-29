09c41b048f052f1add89af26df5bee3aa9156294
darco2018 <newsecond@outlook.com>
2021-12-29 19:58:49 +0100

    convert to lambdas CorpusCSVFileParser
    -
    -
    -

--- src/main/java/com/ust/wordmaster/dictionary/CorpusCSVFileParser.java
+++ src/main/java/com/ust/wordmaster/dictionary/CorpusCSVFileParser.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -14,18 +15,15 @@ public class CorpusCSVFileParser {
 
     public static List<DictionaryEntry> parse(String filePath) {
 
-        List<DictionaryEntry> entries = new ArrayList<>();
         try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
-            lines.forEach(line -> {
-                String[] items = line.split(",");
-                DictionaryEntry entry = createDictionaryEntry(items);
-                entries.add(entry);
-            });
+            return lines
+                    .map(line -> createDictionaryEntry(line.split(",")))
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return entries;
+       return new ArrayList<>();
     }
 
     private static DictionaryEntry createDictionaryEntry(String[] entryData) {
