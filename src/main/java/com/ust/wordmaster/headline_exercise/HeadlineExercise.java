e6087a34e0c90edf62a87b47fd26367726e626bc
darco2018 <newsecond@outlook.com>
2022-02-11 17:25:40 +0100

    Add HeadlineExercise model & DTO

---
+++ src/main/java/com/ust/wordmaster/headline_exercise/HeadlineExercise.java
@@ -0,0 +1,59 @@
+package com.ust.wordmaster.headline_exercise;
+
+import com.ust.wordmaster.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "headline_exercise")
+@Entity
+public class HeadlineExercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "headline_exercise_generator")
+    @SequenceGenerator(name = "headline_exercise_generator", sequenceName = "headline_exercise_seq", initialValue = 10, allocationSize = 1)
+    @Column(name = "headline_exercise_id", nullable = false, updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    /*
+    The <?> lives in the <?>. #[lion, jungle]
+    Do people <?> apples? #[eat]
+    Never play with <?> and <?>!!! #[spiders, insects]
+    */
+
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime dateCreated;
+
+    @Column(nullable = false)
+    private OffsetDateTime lastUpdated;
+
+    @PrePersist
+    void onPersist() {
+        this.dateCreated = OffsetDateTime.now();
+        this.lastUpdated = this.dateCreated;
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        this.lastUpdated = OffsetDateTime.now();
+    }
+
+}
