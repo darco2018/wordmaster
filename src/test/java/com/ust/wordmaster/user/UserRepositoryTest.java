877bf21c98f2de36a272c56437d581b497da1bec
darco2018 <newsecond@outlook.com>
2022-02-19 12:39:08 +0100

    test UserRepository

---
+++ src/test/java/com/ust/wordmaster/user/UserRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.ust.wordmaster.user;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
+public class UserRepositoryTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void whenFindByEmail_thenReturnUser() {
+
+        User user = new User();
+        user.setEmail("999@mai.com");
+        entityManager.persist(user);
+        entityManager.flush();
+
+        User found = userRepository.findByEmail(user.getEmail());
+
+        Assertions.assertThat(found.getEmail())
+                .isEqualTo(user.getEmail());
+    }
+
+}
